4d905458fbc83055e04050a4a4e81726b372b7b4
dj95 <Daniel.Jankowski@rub.de>
2016-01-11 16:52:07 +0100

    added main functionality

--- README.md
+++ README.md
@@ -15,17 +15,16 @@ Usage
 
 - Open the pages.py-file with a text-editor
 - Insert your cookie's express.sid
-- Edit the BASE_URL and the slugs of your used categories
+- Edit the BASE_URL, destination  and the slugs of your used categories
 - Save the file and execute the script with `./pages.py`
 
 
 TODO
 ----
 
-- Save fetched data
-- Generate table of contents
 - Generate pdf from html
-- Refurbish data
+- Automatically fetch image urls
+- Improve appearance of fetched html
 - Error-Testing
 
 
--- pages.py
+++ pages.py
@@ -10,13 +10,56 @@ import urllib.request
 import urllib.parse
 import http.cookiejar
 import colorama
-from colorama import Fore, Back, Style
 
+from colorama import Fore, Back, Style
+from html.parser import HTMLParser
 
 BASE_URL = 'http://linux-statt-windows.org/api/'
-SLUGS = ['111/team-protokoll']
+SLUGS = ['111/team-protokoll', '111/team-protokoll?page=2']
 COOKIE = ""
+DEST = './Pages/'
 
+# Refurbish data
+
+class html_parser(HTMLParser):
+
+    def __init__(self):
+        super().__init__()
+        self.__is_title = False
+        self.titles = []
+
+    def handle_starttag(self, tag, attrs):
+        if tag == 'h3':
+            self.__is_title = True
+
+    def handle_data(self, data):
+        if self.__is_title:
+            self.__is_title = False
+            self.titles.append(data)
+
+
+def generate_toc(toc, titles):
+    doc = '<!DOCTYPE html>\n<head><link rel="stylesheet" href="./style.css"></head><body>'
+    doc += '<center><h1>Team-Handbuch</h1></center><br /><h3>Inhaltsverzeichnis</h3><ul>'
+    for t in titles:
+        doc += '<li><a href="./' + t + '.html">' + toc[t][0] + '</a></li><ul>'
+        #print(toc[t][0])
+        for x in toc[t][1]:
+            doc += '<li>' + x + '</li>'
+            #print('--' + x)
+        doc += '</ul>'
+    doc += '</ul></body>'
+    save_page(doc, 'toc')
+    return
+
+# File handler
+
+def save_page(data, title):
+    with open(DEST + title + '.html', 'w') as fp:
+        fp.write(data)
+
+
+# User authentication
 
 def authenticate():
     cj = http.cookiejar.CookieJar()
@@ -25,9 +68,10 @@ def authenticate():
     return urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
 
 
-def fetch_category(opener, table, not_working_urls):
-    log('fetched content-table')
+# Fetch functions
 
+def fetch_category(opener, table, not_working_urls, toc, titles):
+    parser = html_parser()
     for t in table:
         log('fetching ' + t['title'])
         d = fetch_data(opener, BASE_URL + 'topic/' + str(t['slug']))
@@ -35,8 +79,17 @@ def fetch_category(opener, table, not_working_urls):
             log_error('failed to fetch')
             not_working_urls.append(t['slug'])
         else:
-            print(d['posts'][0]['content'])
-    return not_working_urls
+            # save to file
+            html = '<!DOCTYPE>\n<html>\n<head><link rel="stylesheet" href="./style.css"></head>\n<body><center><h1>' + t['title'] + '</h1></center>''<a href="./toc.html">../Inhaltsverzeichnis</a><hr />'
+            html += d['posts'][0]['content']
+            html += '</body>'
+            save_page(html, t['title'])
+            
+            parser.feed(html)
+            titles.append(t['title'])
+            toc[t['title']] = (t['title'], parser.titles)
+            parser.titles = []
+    return not_working_urls, toc, titles
 
 
 def fetch_data(opener, url):
@@ -49,7 +102,7 @@ def fetch_data(opener, url):
 
 
 def fetch_content_table(opener, slug):
-    rqst = opener.open(BASE_URL + 'api/category/' + slug)
+    rqst = opener.open(BASE_URL + 'category/' + slug)
     data = json.loads(rqst.read().decode('utf-8'))
     topics = data['topics']
     table = []
@@ -57,6 +110,8 @@ def fetch_content_table(opener, slug):
         table.append({'title':t['title'], 'slug':t['slug']})
     return table
 
+
+# Log functions
 
 def log(text):
     print(Fore.GREEN + '==>' + Fore.WHITE + ' ' + text)
@@ -71,17 +126,21 @@ def main():
     log('trying to authenticate')
     opener = authenticate()
     log('Success! Authenticatd')
-   
+
     not_working_urls = []
+    toc, titles = {}, []
 
     for slug in SLUGS:
-        table = fetch_content_table(opener, slug)
         log('fetched content-table')
-
-        not_working_urls = fetch_category(opener, table, not_working_urls)
-
+        table = fetch_content_table(opener, slug)
+        not_working_urls, toc, titles = fetch_category(opener, table, not_working_urls, toc, titles)
+    generate_toc(toc, titles)
+    log('generated table of contents')
     log('not working slugs')
-    print(not_working_urls)
+    if not_working_urls == []:
+        print('None')
+    else:
+        print(not_working_urls)
     return
 
 
